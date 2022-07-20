68ea71e4c09442cb66959532523785dc0ebce769
João Gomes <gomesrodrigues.joaovitor6@gmail.com>
2022-07-20 19:19:34 -0300

    variaveis

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log("eaee")
+
+// const nome = "João"
+// console.log(nome)
+
+// const sobrenome ="Vitor"
+// console.log(sobrenome)
+
+// let idade = 23
+// console.log(idade)
+
+// idade = 23.4
+// console.log(idade)
+
+// console.log(nome, sobrenome, idade)
+
+
+ /* console.log("Olá meu nome é",nome, "e meu sobrenome é", sobrenome, "e minha idade é",idade) */
+
+let vpet = false
+const pet = null
+console.log(typeof pet)
+console.log(typeof vpet)
